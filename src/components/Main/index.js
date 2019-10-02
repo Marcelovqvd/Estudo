9057c70e22f1238ca2c7869622228794aa164672
marcelovqvd <marcelovqvd@gmail.com>
2019-10-02 15:16:45 -0300

    axios e baseURL

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,5 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function Main() {
-  return <h1>Main</h1>
+import api from '../../services/api';
+
+export default class Main extends Component {
+
+
+  render() {
+    return <h1>Main</h1>
+  }
 }
